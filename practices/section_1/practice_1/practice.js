9601f31c37f666e8691a9308e8b9d110360061fd
shary <1287641592@qq.com>
2016-07-27 14:22:05 +0800

    shiyue section_1/practice_1/practice

--- practices/section_1/practice_1/practice.js
+++ practices/section_1/practice_1/practice.js
@@ -1,5 +1,8 @@
 function collect_same_elements(collection_a, collection_b) {
   //在这里写入代码
+  return collection_a.filter((itema)=> {
+    return exist = collection_b.find((itemb)=>itema === itemb);
+  });
 }
 
 module.exports = collect_same_elements;
