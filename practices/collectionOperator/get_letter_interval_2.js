7cf968235f2d6ef0d136c1975e4e7d91779457e1
shary <1287641592@qq.com>
2016-07-27 14:02:22 +0800

    shiyue get_letter_interval_2

--- practices/collectionOperator/get_letter_interval_2.js
+++ practices/collectionOperator/get_letter_interval_2.js
@@ -1,7 +1,27 @@
-'use strict';
+'use strict'
 
+function getNumberArr(number_a, number_b) {
+
+  let arr = [];
+  if (number_a < number_b) {
+    arr = Array.from({length: number_b - number_a + 1}, (v, k)=>k + number_a);
+  } else {
+    arr = Array.from({length: number_a - number_b + 1}, (v, k)=>number_a - k);
+  }
+  return arr;
+}
 function get_letter_interval_2(number_a, number_b) {
   //在这里写入代码
+  let arr = [];
+  arr = getNumberArr(number_a, number_b);
+  return arr.map((item)=> {
+    if (parseInt((item - 1) / 26) === 0) {
+      return String.fromCharCode(item - 1 + 97);
+    }
+    else {
+      return String.fromCharCode(parseInt((item - 1) / 26) - 1 + 97) + String.fromCharCode((item - 1) % 26 + 97);
+    }
+  });
 }
 
 module.exports = get_letter_interval_2;
