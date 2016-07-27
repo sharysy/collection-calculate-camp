3fa5633508e4619aee2e4fc50131f7ee05e18dbd
shary <1287641592@qq.com>
2016-07-27 17:46:50 +0800

    shiyue compute_median

--- practices/reduce/compute_median.js
+++ practices/reduce/compute_median.js
@@ -2,6 +2,11 @@
 
 function compute_median(collection) {
   //在这里写入代码
+let arr=collection.sort((a,b)=>a-b);
+  if(arr.length%2===0){
+    return (arr[parseInt(arr.length/2)-1]+arr[parseInt(arr.length/2)])/2;
+  }
+  else return arr[parseInt(arr.length/2)-1];
 }
 
 module.exports = compute_median;
