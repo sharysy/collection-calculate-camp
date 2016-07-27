834afeed5ab7f6f28224c56ebd7d5f587d71dedf
shary <1287641592@qq.com>
2016-07-27 11:53:24 +0800

    shiyue get_union

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -2,6 +2,8 @@
 
 function get_union(collection_a, collection_b) {
   //在这里写入代码
+  let arr = collection_a.concat(collection_b);
+  return arr.filter((item, pos)=>arr.indexOf(item) === pos);
 }
 
 module.exports = get_union;
