e10677e5670c12a377a2dc7f6d2abcdb76ad80a7
shary <1287641592@qq.com>
2016-07-27 11:38:59 +0800

    shiyue get_integer_interval_2

--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -2,6 +2,13 @@
 
 function get_integer_interval_2(number_a, number_b) {
   //在这里写入代码
+  let arr=[];
+  if (number_a < number_b) {
+    arr= Array.from({length: number_b - number_a + 1}, (v, k)=>k + number_a);
+  } else {
+    arr=Array.from({length: number_a - number_b + 1}, (v, k)=>number_a - k);
+  }
+  return arr.filter((item)=>item%2===0);
 }
 
 module.exports = get_integer_interval_2;
