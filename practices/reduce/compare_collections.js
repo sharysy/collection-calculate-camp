5e9ab047ec8fc9ffb5b73daeabb58155aee83e74
shary <1287641592@qq.com>
2016-07-27 17:03:46 +0800

    shiyue compare_collections

--- practices/reduce/compare_collections.js
+++ practices/reduce/compare_collections.js
@@ -2,6 +2,19 @@
 
 function compare_collections(collection_a, collection_b) {
   //在这里写入代码
+  let flag=true;
+  if (collection_a.length === collection_b.length) {
+    for (let i = 0; i < collection_a.length; i++) {
+      if(collection_a[i] !== collection_b[i]){
+        return false;
+        flag = false;
+      }
+    }
+    if(flag===true){
+      return true;
+    }
+  }
+  else return false;
 }
 
 module.exports = compare_collections;
