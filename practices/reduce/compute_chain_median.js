fce1e9964e65cbceedba452dd4277c645965d1e8
shary <1287641592@qq.com>
2016-07-27 17:38:39 +0800

    shiyue compute_chain_median

--- practices/reduce/compute_chain_median.js
+++ practices/reduce/compute_chain_median.js
@@ -2,6 +2,9 @@
 
 function compute_chain_median(collection) {
   //在这里写入代码
+  let arr = collection.split('->').map((item)=>parseInt(item));
+  let sortedArr = arr.sort((a, b)=>a - b);
+  return sortedArr.length % 2 === 0 ? (sortedArr[parseInt(sortedArr.length / 2)-1] + sortedArr[parseInt(sortedArr.length / 2)]) / 2:sortedArr[parseInt(sortedArr.length / 2)-1];
 }
 
 module.exports = compute_chain_median;
