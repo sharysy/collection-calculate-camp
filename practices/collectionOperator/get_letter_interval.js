92ae0b9a4781ec6f4afe890982dbb5a99d78acc8
shary <1287641592@qq.com>
2016-07-27 11:42:34 +0800

    shiyue get_letter_interval

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -2,6 +2,12 @@
 
 function get_letter_interval(number_a, number_b) {
   //在这里写入代码
+  if (number_a < number_b) {
+    return Array.from({length: number_b - number_a + 1}, (v, k)=>number_a + k);
+  }
+  else {
+    return Array.from({length: number_a - number_b + 1}, (v, k)=>number_a - k);
+  }
 }
 
 module.exports = get_letter_interval;
