d492eb6103e97c04cee6223fc3feeeb4c220f517
shary <1287641592@qq.com>
2016-07-27 11:49:15 +0800

    shiyue get_letter_interval

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -2,12 +2,19 @@
 
 function get_letter_interval(number_a, number_b) {
   //在这里写入代码
+  /*if (number_a < number_b) {
+   return Array.from({length: number_b - number_a + 1}, (v, k)=>number_a + k);
+   }
+   else {
+   return Array.from({length: number_a - number_b + 1}, (v, k)=>number_a - k);
+   }*/
+  let arr = [];
   if (number_a < number_b) {
-    return Array.from({length: number_b - number_a + 1}, (v, k)=>number_a + k);
+    arr = Array.from({length: number_b - number_a + 1}, (v, k)=>number_a + k);
+  } else {
+    arr = Array.from({length: number_a - number_b + 1}, (v, k)=>number_a - k);
   }
-  else {
-    return Array.from({length: number_a - number_b + 1}, (v, k)=>number_a - k);
-  }
+  return arr.map((item)=>String.fromCharCode(item-1+97));
 }
 
 module.exports = get_letter_interval;
